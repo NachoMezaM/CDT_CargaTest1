662cc0a828b087121c1edf0e6bc8ae7360d50276
Benyiman <89081846+Benyiman@users.noreply.github.com>
2024-04-29 10:38:02 -0400

    soluciones del merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,10 @@ import { CrearProfeComponent } from './profesor/crear-profe/crear-profe.componen
 import { IndexProfeComponent } from './profesor/index-profe/index-profe.component';
 import { ViewProfeComponent } from './profesor/view-profe/view-profe.component';
 import {EditarProfeComponent} from './profesor/editar-profe/editar-profe.component'
+import { MenuComponent } from './menu/menu.component';
+import { BarranavegacionComponent } from './barranavegacion/barranavegacion.component';
 export const routes: Routes = [
+    
       { path: '', redirectTo: 'menu', pathMatch: 'full'},
       { path: 'post/index', component: IndexComponent },
       { path: 'post/postId/view', component: ViewComponent },
@@ -15,7 +18,11 @@ export const routes: Routes = [
       { path: 'post/postId/edit', component: EditComponent },
       { path: 'profesor/crear-profe', component: CrearProfeComponent },
       { path: 'profesor/index-profe', component: IndexProfeComponent},
-      { path: 'profesor/view-profe', component: ViewProfeComponent},
-      { path: 'profesor/editar-profe', component: EditarProfeComponent }
+      { path: 'profesor/:profesoridProfesor/view-profe', component: ViewProfeComponent},
+      { path: 'profesor/:profesoridProfesor/editar-profe', component: EditarProfeComponent },
+      { path: 'menu', component: MenuComponent  },
+      { path: 'barranavegacion', component: BarranavegacionComponent}
 
   ];
+
+
