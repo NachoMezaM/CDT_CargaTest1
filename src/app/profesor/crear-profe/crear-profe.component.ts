8d1f6b323d996ab53223f3a557047d3691b2545b
Benyiman <89081846+Benyiman@users.noreply.github.com>
2024-04-29 10:48:10 -0400

    Merge conpletado

--- src/app/profesor/crear-profe/crear-profe.component.ts
+++ src/app/profesor/crear-profe/crear-profe.component.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
   
 import { ProfesorService } from '../profesor.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { BarranavegacionComponent } from "../../barranavegacion/barranavegacion.component";
 
 @Component({
-  selector: 'app-crear-profe',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './crear-profe.component.html',
-  styleUrl: './crear-profe.component.css'
+    selector: 'app-crear-profe',
+    standalone: true,
+    templateUrl: './crear-profe.component.html',
+    styleUrl: './crear-profe.component.css',
+    imports: [CommonModule, ReactiveFormsModule, RouterOutlet, RouterLink, BarranavegacionComponent]
 })
 export class CrearProfeComponent {
   
   
     form!: FormGroup;
-        
+    mensajeRespuesta: string = '';
     /*------------------------------------------
     --------------------------------------------
     Crear constructor
@@ -41,6 +42,13 @@ export class CrearProfeComponent {
         Horas: new FormControl('', [Validators.required]),
         ValorHora: new FormControl('', [Validators.required]),
         idJerarquia: new FormControl('', [Validators.required]),
+        Direccion: new FormControl('', [Validators.required]),
+        Telefono: new FormControl('', [Validators.required]),
+        Grado: new FormControl('', [Validators.required]),
+        TituloGrado: new FormControl('', [Validators.required]),
+        Estado: new FormControl('', [Validators.required]),
+        Apellido: new FormControl('', [Validators.required]),
+
       });
     }
         
@@ -58,12 +66,27 @@ export class CrearProfeComponent {
      *
      * @return response()
      */
-    submit(){
+    submit() {
       console.log(this.form.value);
-      this.profesorService.create(this.form.value).subscribe((res:any) => {
-           console.log('profesor creado successfully!');
-           this.router.navigateByUrl('profesor/index-profe');
-      })
+      this.profesorService.create(this.form.value).subscribe(
+        (res: any) => {
+          console.log('Profesor creado exitosamente!');
+          this.mensajeRespuesta = 'Profesor creado exitosamente';
+          setTimeout(() => {
+            this.router.navigateByUrl('profesor/index-profe');
+          }, 2000); // Redirige después de 2 segundos
+        },
+        error => {
+      console.error('Error al crear profesor:', error);
+      if (error && error.error === 'Ya existe un profesor con ese ID') {
+        this.mensajeRespuesta = 'Ya existe un profesor con ese ID. Por favor, elige otro ID.';
+      } else if (error && error.error && typeof error.error === 'string') {
+        this.mensajeRespuesta = 'Error al crear profesor: ' + error.error;
+      } else {
+        this.mensajeRespuesta = 'Error al crear profesor. Por favor, verifique que estan correctos los datos.';
+      }
+    }
+  );
     }
     
   }
