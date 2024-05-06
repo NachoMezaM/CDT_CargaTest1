dab7cbae4b314fa5b627e5c70e9ae1e9c156f123
NachoMezaM <nachom1605@gmail.com>
2024-05-06 17:02:41 -0400

    a

--- src/app/post/seccion/seccion.component.ts
+++ src/app/post/seccion/seccion.component.ts
@@ -19,13 +19,6 @@ export class SeccionComponent {
   post!: Post;
   form!: FormGroup;
   
-  facultades = [
-    { id: 'FI', nombre: 'Facultad de Ingenieria y Negocios', carreras: ['Facultad','Agronomia', 'Contador Auditor Online','Ingenieria Civil Industrial','Ingenieria Civil Informatica','Ingenieria Comercial'] },
-    { id: 'FS', nombre: 'Facultad de Salud', carreras: ['Facultad','Enfermería', 'Nutricion y Dietetica','Obstetricia y Puericultura','Quimica y Farmacia','Tecnico de Nivel Superior en Enfermeria','Terapia Ocupacional'] },
-    { id: 'FE', nombre: 'Facultad de Educacion', carreras: ['Facultad','Educación Parvularia', 'Licenciatura en Educacion','Pedagogía en Educación Diferencial','Pedagogía en Educación Física','Pedagogía en Educación General Básica','Pedagogía en Inglés','Pedagogía en Música'] },
-    { id: 'TE', nombre: 'Facultad de Teologia', carreras: ['Teología'] },
-    { id: 'DE', nombre: 'Facultad de Ciencias Juridicas y Sociales', carreras: ['Facultad','Derecho', 'Psicología','Trabajo Social','Licenciatura en Trabajo Social',] },
-  ];
 
   constructor(
     public postService: PostService,
@@ -38,21 +31,8 @@ export class SeccionComponent {
     this.postService.find(this.id).subscribe((data: Post)=>{
       this.post = data;
     }); 
-    this.form = new FormGroup({
-      idAsignatura: new FormControl('', [Validators.required]),
-      Facultad: new FormControl('', [Validators.required]),
-      Carrera: new FormControl('', [Validators.required]),
-    });
-  }
 
-  get Carreras() {
-    const facultadControl = this.form.get('Facultad');
-    if (facultadControl && facultadControl.touched) {
-      const facultadId = facultadControl.value;
-      const facultad = this.facultades.find(f => f.id === facultadId);
-      return facultad? facultad.carreras : [];
-    }
-    return [];
-  }
 
+
+  }
 }
