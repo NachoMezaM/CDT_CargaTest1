1ff450d246174bda819ea54478ebbd6434c78028
NachoMezaM <nachom1605@gmail.com>
2024-06-05 11:20:38 -0400

    Seccion Fix Carreras

--- src/app/post/seccion/seccion.component.ts
+++ src/app/post/seccion/seccion.component.ts
@@ -26,6 +26,7 @@ export class SeccionComponent {
   form2!: FormGroup;
   facultades = [
     { id: 'FI', nombre: 'Facultad de Ingenieria y Negocios', carreras: [
+        { nombre: 'Seleccione Carrera', valor: '' },
         { nombre: 'FAIN', valor: 'FI' },
         { nombre: 'Agronomia', valor: 'AG' },
         { nombre: 'Contador Auditor Online', valor: 'CO' },
@@ -34,6 +35,7 @@ export class SeccionComponent {
         { nombre: 'Ingenieria Comercial', valor: 'IC' }
     ] },
     { id: 'FS', nombre: 'Facultad de Salud', carreras: [
+        { nombre: 'Seleccione Carrera', valor: '' },
         { nombre: 'Facultad', valor: 'FS' },
         { nombre: 'Enfermería', valor: 'EN' },
         { nombre: 'Nutricion y Dietetica', valor: 'NU' },
@@ -43,6 +45,7 @@ export class SeccionComponent {
         { nombre: 'Terapia Ocupacional', valor: 'TO' }
     ] },
     { id: 'FE', nombre: 'Facultad de Educacion', carreras: [
+        { nombre: 'Seleccione Carrera', valor: '' },
         { nombre: 'Facultad', valor: 'FE' },
         { nombre: 'Educación Parvularia', valor: 'EP' },
         { nombre: 'Licenciatura en Educacion', valor: 'LE' },
@@ -53,6 +56,7 @@ export class SeccionComponent {
         { nombre: 'Pedagogía en Música', valor: 'MU' },
     ] },
     { id: 'DE', nombre: 'Facultad de Ciencias Juridicas y Sociales', carreras: [
+        { nombre: 'Seleccione Carrera', valor: '' },
         { nombre: 'Facultad', valor: 'FD' },
         { nombre: 'Derecho', valor: 'DE' },
         { nombre: 'Psicología', valor: 'PS' },
@@ -60,9 +64,11 @@ export class SeccionComponent {
         { nombre: 'Licenciatura en Trabajo Social', valor: 'LS' }
     ] },
     { id: 'TE', nombre: 'Facultad de Teologia', carreras: [
+        { nombre: 'Seleccione Carrera', valor: '' },
         { nombre: 'Teologia', valor: 'TE' }
     ] },
     { id: 'XX', nombre: 'Complementario', carreras: [
+      { nombre: 'Seleccione Carrera', valor: '' },
       { nombre: 'Complementario', valor: 'CO' }
   ] }
 ];
